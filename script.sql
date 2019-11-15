a447a57c1c41e4a1584679bb00b0896863c3336b
Axel Redois <axelredois25@gmail.com>
2019-11-15 14:27:50 +0100

    change database script and add country method

--- script.sql
+++ script.sql
@@ -14,12 +14,13 @@ CREATE TABLE countries (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(150) NOT NULL,
     continents_id INT NOT NULL,
-    FOREIGN KEY (continents_id) REFERENCES continents(id)                   
+    image TEXT,
+    FOREIGN KEY (continents_id) REFERENCES continents(id)
 );
 CREATE TABLE articles (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL ,
-    image VARCHAR(255),
+    image TEXT,
     date DATE,
     content MEDIUMTEXT,
     categories_id INT,
@@ -35,13 +36,6 @@ CREATE TABLE comments (
     articles_id INT,
     FOREIGN KEY (articles_id) REFERENCES articles(id)
 );
+
 INSERT INTO continents (name) VALUES ('Afrique'), ('Amerique du Nord'),('Amerique du Sud'), ('Asie'), ('Europe'), ('Oceanie');
-INSERT INTO countries (name, continents_id) VALUES ('Maroc', 1), ('Tunisie', 1), ('Nigeria', 1), ('Cameroun', 1), ('Mozambique', 1);
-INSERT INTO countries (name, continents_id) VALUES ('Etats-Unis', 2), ('Canada', 2), ('Alaska', 2), ('Mexique', 2);
-INSERT INTO countries (name, continents_id) VALUES ('Perou', 3), ('Argentine', 3), ('Brésil', 3), ('Colombie', 3), ('Chili', 3);
-INSERT INTO countries (name, continents_id) VALUES ('Japon', 4), ('Mongolie', 4), ('Vietnam', 4), ('Thailande', 4), ('Corée du Sud', 4);
-INSERT INTO countries (name, continents_id) VALUES ('France', 5), ('Italie', 5), ('Espagne', 5), ('Portugal', 5), ('Croatie', 5);
-INSERT INTO countries (name, continents_id) VALUES ('Australie', 6), ('Nouvelle-Zelande', 6), ('Philippines', 6);
-INSERT INTO categories(name) VALUES ('Destinations'), ('Gastronomie'), ('Logement');
-INSERT INTO articles(title, date, content, categories_id, countries_id) VALUES ('Article traitant de la gastronomie Chilienne', '2019-10-18',
-            'Voici du contenu pour créer une base de données superbement bien établie avec tout plein texte qui parle de bouffe au Chili', '2', 14 );
+
