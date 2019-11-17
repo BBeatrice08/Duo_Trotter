cb85457c4b0d21e4453cd4472d4f9d9857ee7e69
Beatrice Beauperin <beauperin.beatrice@gmail.com>
2019-11-17 16:22:30 +0100

    articles name available in admin comments list

--- src/Model/CommentsManager.php
+++ src/Model/CommentsManager.php
@@ -11,6 +11,20 @@ class CommentsManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
+    public function listComment(): array
+    {
+        $request = $this->pdo->prepare("
+        SELECT c.date, c.user_name, c.content, a.title
+        FROM ".self::TABLE." c
+            JOIN ".ArticlesManager::TABLE." a ON c.articles_id = a.id
+            ORDER BY a.id DESC
+        ");
+
+        $request->execute();
+
+        return $request->fetchAll();
+    }
+
     public function insertComment(array $comments): bool
     {
         $request = $this->pdo->prepare("INSERT INTO ".self::TABLE." (user_name, date, content, articles_id) VALUES
